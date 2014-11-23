e5cc30333719cac26ae96e773680a5f1b9803201
Scott Walters <srwalte2@illinois.edu>
2014-11-23 16:04:29 -0600

    Started writing test message sending for android

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
+/**
+ * IMPORTS
+ */
 var gcm = require('node-gcm');
-var message = new gcm.Message();
+var secrets = require('./secrets.js');
 
-// API Server Key
+function main(){
+    var message = new gcm.Message();
+    var sender = new gcm.Sender(secrets.gcm.apikey);
+    var registrationIds = [];
+
+    // Value the payload data to send
+    message.addData('message', 'test');
+    message.addData('title', 'Push Notification Test');
+    message.addData('msgcnt', '2');
+    message.collapseKey = 'demo';
+    message.delayWhileIdle = true;
+    message.timeToLive = 30 * 60; // 30 minutes
+
+    // At least one reg id require
+
+}
+
+main();
