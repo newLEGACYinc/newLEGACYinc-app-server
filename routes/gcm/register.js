0eb5957871e27ed49b09f81178f9355bbc357fd8
Scott Walters <srwalte2@illinois.edu>
2014-11-27 12:56:20 -0600

    Minor refactor

--- routes/gcm/register.js
+++ routes/gcm/register.js
@@ -1,7 +1,7 @@
 module.exports = function(db){
 	return function(req, res){
-		var registrationId = req.body.registrationId;
-		db.gcm.addRegistrationId(registrationId, function(err, results){
+		var id = req.body.id;
+		db.gcm.addRegistrationId(id, function(err, results){
 			if (err && err.code !== 'ER_DUP_ENTRY'){
 				console.log(err);
 				return res.status(500).send(err);
@@ -10,4 +10,3 @@ module.exports = function(db){
 		});
 	};
 };
-
