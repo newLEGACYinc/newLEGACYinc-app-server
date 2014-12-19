8c095c6ef0439995c53fdd4f343f5442a807319c
Scott Walters <srwalte2@illinois.edu>
2014-12-19 15:41:06 -0600

    Announcements working again

--- routes/sendMessage.js
+++ routes/sendMessage.js
@@ -1,7 +1,7 @@
 module.exports = function(sender){
 	return function(req,res){
 		if (req.body.title && req.body.message){
-			sender.send(req.body.title, req.body.message, 'announce');
+			sender.send(req.body.title, req.body.message, 'announcements');
 			res.send("ok");
 		} else {
 			res.status(400).send("missing title or message");
