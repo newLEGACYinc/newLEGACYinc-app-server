57cb28d90caf1e5d6f8d2ffb0ffb2005d3b7e4cf
Scott Walters <me@scowalt.com>
2016-07-04 18:47:33 -0700

    Fixed routing issue

--- src/routes/data/index.js
+++ src/routes/data/index.js
@@ -1,13 +1,16 @@
 module.exports = function() {
+	var express = require( 'express' );
+	var router = express.Router();
+
 	var instagram = require( __dirname + '/instagram' )();
 	var twitch = require( __dirname + '/twitch' )();
 	var twitter = require( __dirname + '/twitter' )();
 	var youTube = require( __dirname + '/youTube' )();
 
-	return {
-		instagram: instagram,
-		twitch: twitch,
-		twitter: twitter,
-		youTube: youTube
-	};
+	router.get( '/instagram', instagram );
+	router.get( '/twitch', twitch );
+	router.get( '/twitter', twitter );
+	router.get( '/youTube', youTube );
+
+	return router;
 };
