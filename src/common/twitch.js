bcf74d0d136e6b8029cee900c88f790524b82f79
Scott Walters <me@scowalt.com>
2016-10-09 14:37:06 -0700

    newapi

--- src/common/twitch.js
+++ src/common/twitch.js
@@ -1,5 +1,5 @@
 module.exports = function() {
-	var TwitchClient = require( 'node-twitchtv' );
+	var TwitchClient = require( 'twitch-sdk' );
 
 	var twitchAccount = {
 		client_id: process.env.TWITCH_CLIENT_ID,
@@ -8,12 +8,24 @@ module.exports = function() {
 
 	console.log( 'client id = ' + process.env.TWITCH_CLIENT_ID );
 
-	var twitchClient = new TwitchClient( twitchAccount );
+	// var twitchClient = new TwitchClient( twitchAccount );
+	TwitchClient.init( twitchAccount, function( error, status ) {
+		console.log( 'twitch init' );
+		console.log( error );
+		console.log( status );
+	} );
 
 	function getProfileInfo( callback ) {
-		twitchClient.streams( { channel: process.env.TWITCH_USERNAME }, function( err, response ) {
-			callback( err, response );
+		TwitchClient.api( { method: 'streams', params: { channel: process.env.TWITCH_USERNAME } }, function( error, list ) {
+			console.log( 'twitch api callback' );
+			console.log( error );
+			console.log( list );
+			callback( list );
 		} );
+
+		// twitchClient.streams( { channel: process.env.TWITCH_USERNAME }, function( err, response ) {
+		// 	callback( err, response );
+		// } );
 	}
 
 	return {
