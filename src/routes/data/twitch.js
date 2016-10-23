797c5a53a42afe469147bd7ac001152d97cd95b8
Scott Walters <me@scowalt.com>
2016-10-23 09:55:16 -0700

    Poking hole in ACAO for chrome extension

--- src/routes/data/twitch.js
+++ src/routes/data/twitch.js
@@ -1,11 +1,14 @@
 module.exports = function( common ) {
 	return function twitchDataGet( req, res ) {
-		common.twitch.getProfileInfo( function( err, response ) {
+		common.twitch.getProfileInfo( function( err, profileInfo ) {
+			// Explicitly allow any chrome extension to access this resource
+			res.header( 'Access-Control-Allow-Origin', 'chrome-extension://*' );
+
 			if ( err ) {
 				console.error( err );
 				res.status( 500 ).send();
 			} else {
-				res.status( 200 ).send( response );
+				res.status( 200 ).send( profileInfo );
 			}
 		} );
 	};
