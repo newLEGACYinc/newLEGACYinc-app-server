8a9780e3c40b9430b9cfa0fb6479588806ef20d7
Scott Walters <me@scowalt.com>
2016-12-15 22:41:44 -0800

    Spelling

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@
 module.exports = function() {
 	// Import libraries
 	var mongoose = require( 'mongoose' );
-	mongoose.connect( proces.env.DB_URL, { config: { autoIndex: false } } );
+	mongoose.connect( process.env.DB_URL, { config: { autoIndex: false } } );
 
 	var Device = mongoose.model( 'Device', {
 		// Indexes
