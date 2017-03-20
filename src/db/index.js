02ba29b6b70d03fbc6c8b3c2f214d10eb333012a
Scott Walters <me@scowalt.com>
2017-03-19 17:30:50 -0700

    Using built-in promise library for mongoose instead of mongoose's (#17)

--- src/db/index.js
+++ src/db/index.js
@@ -2,6 +2,7 @@
 module.exports = function() {
 	// Import libraries
 	var mongoose = require( 'mongoose' );
+	mongoose.Promise = global.Promise;
 	mongoose.connect( process.env.MONGODB_URI );
 
 	var mysql = require( 'mysql' );
