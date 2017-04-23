93039dd77d1e81477c66611c00960b09934e41d7
Scott Walters <me@scowalt.com>
2017-04-22 17:44:52 -0700

    new logging library

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ var app = express();
 var fs = require( 'fs' );
 var http = require( 'http' );
 var bodyParser = require( 'body-parser' );
+require('log4js').replaceConsole();
 
 // MODULE IMPORTS
 var common = require( __dirname + '/common' )();
