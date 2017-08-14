cb5443858c0aecdd1edb69fbfdc29519d17dac00
Scott Walters <me@scowalt.com>
2017-08-13 20:49:12 -0700

    fix build error

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,5 +20,5 @@ module.exports = function( grunt ) {
 	grunt.loadNpmTasks( 'grunt-jscs' );
 
 	grunt.registerTask( 'travis', [ 'test' ] );
-	grunt.registerTask( 'test', [ 'jshint', 'jscs'] );
+	grunt.registerTask( 'test', [ 'jshint', 'jscs' ] );
 };
