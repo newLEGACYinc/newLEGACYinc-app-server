cf91bad1d79433fbdd65c2dd64625dd555583fcb
Scott Walters <me@scowalt.com>
2017-09-04 12:30:10 -0700

    Attempting to fix nested routers

--- src/routes/data/twitch.js
+++ src/routes/data/twitch.js
@@ -1,6 +1,6 @@
 module.exports = function( common ) {
 	var express = require( 'express' );
-	var router = express.Router();
+	var router = express.Router( { mergeParams: true } );
 
 	router.get( '/lastOnline', function twitchGetLastOnline( req, res ) {
 		common.twitch.getLastOnline( function( error, lastOnline ) {
